cf1e1e9dc77eab51217c214bfae460625be4c0c9
André Souza <andresouza_ti@yahoo.com>
2024-11-21 22:51:50 -0300

    troca de numeros para melhor entendimento

--- aula4_operadores_aritimeticos.py
+++ aula4_operadores_aritimeticos.py
@@ -1,5 +1,6 @@
 num1 = 10
 num2 = 2
+num3 = 3
 
 adicao = num1 + num2
 print('Adição:', adicao)
@@ -10,14 +11,14 @@ print('Subtração:', subtracao)
 multiplicacao = num1 * num2
 print('Multiplicação:', multiplicacao)
 
-divisao = num1 / num2
+divisao = num1 / num3
 print('Divisão:', divisao)
 
-divisao_inteira = num1 // num2
+divisao_inteira = num1 // num3
 print('Divisão inteira:', divisao_inteira)
 
 exponenciacao = num1 ** num2
 print('Exponenciação:', exponenciacao)
 
-modulo = 50 % 3 #resto da divisão
+modulo = num1 % num3 #resto da divisão
 print('Módulo:', modulo)
